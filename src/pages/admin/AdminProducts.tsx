0fda612cb8774c213aaea8a39d2ba1d2138a8205
Jhpmel Elisan <jhomelelisan@gmail.com>
2026-02-18 14:38:14 +0800

    .

--- src/pages/admin/AdminProducts.tsx
+++ src/pages/admin/AdminProducts.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import productService from '../../services/product.service';
