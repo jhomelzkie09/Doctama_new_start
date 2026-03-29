386bc7cc96452fd5f7e3101cec635959d78ee69a
Jhpmel Elisan <jhomelelisan@gmail.com>
2026-03-30 06:57:39 +0800

    try

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL?.trim() || 'https://doctamaapisimple-production.up.railway.app/api',
+  API_URL: process.env.REACT_APP_API_URL?.trim() || 'https://doctamaapi-simple.onrender.com',
   ENVIRONMENT: process.env.REACT_APP_ENVIRONMENT || 'production'
 };
 
