026964f52537c7bc36c3d591b165ed1fea11f887
Jhpmel Elisan <jhomelelisan@gmail.com>
2026-04-06 05:28:11 +0800

    delete comment

--- src/pages/customer/Contact.tsx
+++ src/pages/customer/Contact.tsx
@@ -272,7 +272,6 @@ const Contact = () => {
                     <p className="text-xs text-slate-400">Sorsogon City, Philippines</p>
                   </div>
                 </div>
-                // Contact.tsx - Safe Google Maps Embed (this is allowed)
                 <div className="relative h-64 rounded-xl overflow-hidden">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.123456789!2d123.456789!3d12.345678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDIwJzQ0LjgiTiAxMjTCsDI3JzUwLjgiRQ!5e0!3m2!1sen!2sph!4v1234567890"
